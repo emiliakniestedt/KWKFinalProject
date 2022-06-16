680734f28c80339949c964e32848f0cdd911d84d
VallinZ <vickyzhulove88@gmail.com>
2022-06-16 10:30:58 -0500

    update

---
+++ KWKFinalProject/Event.swift
@@ -0,0 +1,8 @@
+//
+//  Event.swift
+//  KWKFinalProject
+//
+//  Created by Vicky Zhu on 6/16/22.
+//
+
+import Foundation
---
+++ KWKFinalProject/EventCell.swift
@@ -0,0 +1,13 @@
+//
+//  EventCell.swift
+//  KWKFinalProject
+//
+//  Created by Vicky Zhu on 6/16/22.
+//
+
+import UIKit
+
+class EventCell: UITableViewCell{
+    @IBOutlet weak var eventLabel: UILabel!
+}
+
